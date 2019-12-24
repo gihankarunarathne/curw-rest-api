c619f09b61d262dd02f376e84410ff0e396d4717
shasini274 <shasiniumesha24@gmail.com>
2019-12-24 16:39:47 +0530

    Email alert script was added

--- utils/UtilEmailAlert.py
+++ utils/UtilEmailAlert.py
@@ -1,10 +1,20 @@
-from flask_mail import Message
-from app import mail
+import smtplib
 
-def sendemail(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = text_body
-    msg.html = html_body
-    mail.send(msg)
+def send_email(subject, msg):
+    SENDER_EMAIL_ADDRESS = "curwalerts@gmail.com"
+    RECEIVER_EMAIL_ADDRESS = "shasiniumesha274@gmail.com"
+    PASSWORD = "curwalerts@data"
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(SENDER_EMAIL_ADDRESS, PASSWORD)
+        message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.sendmail(SENDER_EMAIL_ADDRESS, RECEIVER_EMAIL_ADDRESS, message)
+        server.quit()
+        print("Success: Email sent!")
+    except:
+        print("Email failed to send.")
 
 
